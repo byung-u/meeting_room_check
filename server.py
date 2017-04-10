b0e8506c6c67cf1f5405cbc80dd5d8ba7318ca80
byung-u <iam.byungwoo@gmail.com>
2017-04-10 19:43:19 +0900

    ADD calcuation

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 #!/usr/local/bin/python3
 from redis import StrictRedis
 from redis_collections import List
+from functools import reduce
 
 import matplotlib.pyplot as pp
 import os
@@ -19,9 +20,18 @@ def redis_init():
 
 def main():
     r = redis_init()
-    print(r[:-1])  # get value.. what a great
     pp.plot(r)
     pp.show()
+"""
+    light = r[-100:-1]
+    light.sort()
+    print(light)
+    cl = light[40:60]
+    print(cl)
+    sum_cl = 0
+    print(reduce(lambda x, y: x + y, cl) / len(cl))
+"""
+    #print(r[:-1])  # get value.. what a great
 
 
 if __name__ == '__main__':
